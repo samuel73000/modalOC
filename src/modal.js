7830f755c6ae400e299fded1e1edeef780d71c09
Samuel Pouard <thosam73000@gmail.com>
2025-02-21 11:06:03 +0100

    ModalOC

--- src/modal.js
+++ src/modal.js
@@ -1,7 +1,8 @@
 import "./modal.css";
-import { useState } from 'react';
-import { createPortal } from 'react-dom';
+import { useState } from "react";
+import { createPortal } from "react-dom";
 import Button from "@mui/material/Button";
+import React from "react";
 
 export default function PortalExample(props) {
   const [showModal, setShowModal] = useState(false);
@@ -9,23 +10,35 @@ export default function PortalExample(props) {
   // Appeler la fonction qui est passée en prop, si elle existe
   const handleButtonClick = () => {
     if (props.onOpen) {
-      const isValid = props.onOpen(); 
-      if (!isValid) return; 
+      const isValid = props.onOpen();
+      if (!isValid) return;
     }
-    setShowModal(true); 
+    setShowModal(true);
   };
   return (
-    <>
-      <Button variant='contained' onClick={handleButtonClick}>{props.nameButton}</Button>
-      {showModal && createPortal(
-        <div className="modal-overlay" onClick={() => setShowModal(false)}>
-          <div className="modal" onClick={(e) => e.stopPropagation()}>
-            <div>{props.texteModal}</div>
-            <Button variant='contained' onClick={() => setShowModal(false)}>Close</Button>
-          </div>
-        </div>,
-        document.body
-      )}
-    </>
+    <React.Fragment>
+      <Button variant='contained' onClick={handleButtonClick}>
+        {props.nameButton}
+      </Button>
+      {showModal &&
+        createPortal(
+          <div className='modal-overlay' onClick={() => setShowModal(false)}>
+            <div className='modal' onClick={(e) => e.stopPropagation()}>
+              <div>{props.texteModal}</div>
+              <Button variant='contained' onClick={() => setShowModal(false)}>
+                Close
+              </Button>
+            </div>
+          </div>,
+          document.body
+        )
+        }
+    </React.Fragment>
   );
 }
+
+
+
+
+
+
